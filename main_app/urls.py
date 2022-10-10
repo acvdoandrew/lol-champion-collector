3745c9b2efd2cd7b848390f642c8083d32ce25de
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-10 16:39:02 -0400

    refactos url naming

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('mains/', views.ChampionList.as_view(), name='mains_index'),
-    path('mains/<int:pk>/', views.ChampionDetail.as_view(), name='mains_detail')
+    path('champions/', views.ChampionList.as_view(), name='champions_index'),
+    path('champions/<int:pk>/', views.ChampionDetail.as_view(), name='champions_detail')
 ]
