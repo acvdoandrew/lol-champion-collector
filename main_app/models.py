1bf32b20fe7f7d0dd224117913a77d6691a37b59
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-13 20:50:48 -0400

    adds user field to champion and migrates

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class SkinTheme(models.Model):
@@ -18,6 +19,7 @@ class Champion(models.Model):
     epithet = models.TextField(max_length=100)
     hours = models.IntegerField()
     skins = models.ManyToManyField(SkinTheme)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
